45df9c7616c1ceb0b027d8d8ea0a579939808112
Adamo <czerwonskiadam@gmail.com>
2025-01-07 13:58:48 +0100

    add preprocessing

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import os
 import json
 from config import EnvConfig
+import numpy as np
+import pickle
 
 load_dotenv()
 
@@ -25,78 +27,59 @@ mysql_client = MySQLClient(
 
 
 def process_message(message):
-    # Custom logic to process the message
 
-    # need location id here also
     print(f"Processing message: {message}")  # tu jest 
     message = json.loads(message)
     id = get_location_id(message['latitude'], message['longitude'])
     print(f"Location id: {id}")
     
     print("preprocessing")  # use lat long to get right model pickle file
-
+    y = message["cloud_cover"]
+    # preprocessing X here, not relevant right now
 
     # get model
     model = adls_client.load_pickled_model_from_container(
         config.container_name, "initial_model.pkl"
     )
-
     # fit
     print("fitting model")
+    model.learn_one(y)
+    
     # forecast
     print("forecasting")
+    forecast = forecast = np.clip(np.array(model.forecast(4*24)), 0, 100).reshape(-1, 4).mean(axis=1)
+
     # save forecast to ADLS & mysql
-    insert_query = """
-        INSERT INTO cloud_cover_forecasts (
-            location_id, forecast_hour_1, forecast_hour_2, forecast_hour_3, 
-            forecast_hour_4, forecast_hour_5, forecast_hour_6, forecast_hour_7, 
-            forecast_hour_8, forecast_hour_9, forecast_hour_10, forecast_hour_11, 
-            forecast_hour_12, forecast_hour_13, forecast_hour_14, forecast_hour_15, 
-            forecast_hour_16, forecast_hour_17, forecast_hour_18, forecast_hour_19, 
-            forecast_hour_20, forecast_hour_21, forecast_hour_22, forecast_hour_23, 
-            forecast_hour_24
-        ) VALUES (
-            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
-            %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-        )
+    update_query = """
+        UPDATE cloud_cover_forecasts SET
+            forecast_hour_1 = %s, forecast_hour_2 = %s, forecast_hour_3 = %s, 
+            forecast_hour_4 = %s, forecast_hour_5 = %s, forecast_hour_6 = %s, 
+            forecast_hour_7 = %s, forecast_hour_8 = %s, forecast_hour_9 = %s, 
+            forecast_hour_10 = %s, forecast_hour_11 = %s, forecast_hour_12 = %s, 
+            forecast_hour_13 = %s, forecast_hour_14 = %s, forecast_hour_15 = %s, 
+            forecast_hour_16 = %s, forecast_hour_17 = %s, forecast_hour_18 = %s, 
+            forecast_hour_19 = %s, forecast_hour_20 = %s, forecast_hour_21 = %s, 
+            forecast_hour_22 = %s, forecast_hour_23 = %s, forecast_hour_24 = %s
+        WHERE location_id = %s
     """
 
     # Example data for insertion
-    values = (
-        3,  # location_id
-        15,
-        20,
-        25,
-        30,
-        35,
-        40,
-        45,
-        50,
-        55,
-        60,
-        65,
-        70,
-        75,
-        80,
-        85,
-        90,
-        95,
-        100,
-        105,
-        110,
-        115,
-        120,
-        125,
-        130,
-    )
+    values = (id,) + tuple(forecast)
 
-    # # Execute the query
-    # with mysql_client as db:
-    #     db.insert(insert_query, values)
+    # Execute the query
+    with mysql_client as db:
+        db.execute(update_query, values)
+
+
+    # Save the model back to ADLS and the forecast to ADLS
+    # model_pickle = pickle.dumps(model)
+    # adls_client.upload_pickle(
+    #     config.container_name, "initial_model.pkl", model_pickle
+    # )
 
     # adls_client.upload_dict_as_json(
-    #     config.container_name, "forecast.json", json.dumps(values)
-    # )  # results are ml forecast results
+    #     config.container_name, "forecast.json", json.dumps(values+(message['timestamp']))
+    # )  
 
 
 def run_consumer():
