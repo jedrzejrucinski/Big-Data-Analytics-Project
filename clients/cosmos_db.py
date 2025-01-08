4b4071c67b908b725d63d9b3694f6ac631d436d6
jedrzejrucinski <jedrek.rucinski@gmail.com>
2025-01-08 01:53:52 +0100

    fix

--- clients/cosmos_db.py
+++ clients/cosmos_db.py
@@ -13,14 +13,7 @@ class CosmosDBClient:
 
     def add_item(self, item):
         try:
-            self.container.create_item(body=item)
+            self.container_name.create_item(body=item)
             print("Item created successfully")
         except exceptions.CosmosResourceExistsError:
             print("Item already exists")
-
-
-# Usage
-if __name__ == "__main__":
-    client = CosmosDBClient()
-    item = {"id": "1", "your_partition_key": "partition_value", "data": "your_data"}
-    client.add_item(item)
