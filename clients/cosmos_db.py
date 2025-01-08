be9630c6d7a19791b1b8ee68f1363434f9b59390
jedrzejrucinski <jedrek.rucinski@gmail.com>
2025-01-08 01:55:11 +0100

    fix

--- clients/cosmos_db.py
+++ clients/cosmos_db.py
@@ -8,12 +8,14 @@ class CosmosDBClient:
         endpoint = config.cosmosdb_account_host
         key = config.cosmosdb_account_key
         self.client = CosmosClient(endpoint, key)
-        self.database_name = "Big-Data-Results"
-        self.container_name = "satellite_visibility_1"
+        self.database_name = config.cosmosdb_database
+        self.container_name = config.cosmosdb_container
+        self.database = self.client.get_database_client(self.database_name)
+        self.container = self.database.get_container_client(self.container_name)
 
     def add_item(self, item):
         try:
-            self.container_name.create_item(body=item)
+            self.container.create_item(body=item)
             print("Item created successfully")
         except exceptions.CosmosResourceExistsError:
             print("Item already exists")
