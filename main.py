b4302f318df93e93223a981e4980a91c47a74337
jedrzejrucinski <jedrek.rucinski@gmail.com>
2025-01-19 23:26:56 +0100

    add handling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from clients.adls import ADLSClient
 from clients.kafka import KafkaConsumer
 from clients.mysql_client import MySQLClient
@@ -11,6 +12,8 @@ import pickle
 import math
 import datetime
 import pytz
+from concurrent.futures import ThreadPoolExecutor
+from queue import Queue
 
 POF = 1737288000
 
@@ -36,32 +39,37 @@ kafka_consumer = KafkaConsumer(
 )
 adls_client = ADLSClient(config.storage_account_name, config.storage_account_key)
 mysql_client = MySQLClient(
-    config.mysql_host,
-    "weather_admin",
-    config.mysql_password,
-    "weather_db",
+    config.mysql_host, "weather_admin", config.mysql_password, "weather_db"
 )
+
+# Create a thread pool for parallel processing
+executor = ThreadPoolExecutor(max_workers=4)
+
+# Queue for message processing
+message_queue = Queue()
 
 
 def extract_data(message):
-    message = json.loads(message)
-    lat, lon = float(message["latitude"]), float(
-        message["longitude"]
-    )  # this will work but for now it wont match values in the table
-    y = int(message["cloud_coverage"])
-    timestamp = int(message["timestamp"])
+    try:
+        message = json.loads(message)
+        lat, lon = float(message["latitude"]), float(message["longitude"])
+        y = int(message["cloud_coverage"])
+        timestamp = int(message["timestamp"])
 
-    x = {
-        "dt": timestamp,
-        "temp": float(message["temperature"]),
-        "pressure": int(message["pressure"]),
-        "humidity": int(message["humidity"]),
-        "wind_speed": float(message["wind_speed"]),
-        "wind_deg": int(message["wind_direction"]),
-        "precipitation": float(message["precipitation"]),
-    }
+        x = {
+            "dt": timestamp,
+            "temp": float(message["temperature"]),
+            "pressure": int(message["pressure"]),
+            "humidity": int(message["humidity"]),
+            "wind_speed": float(message["wind_speed"]),
+            "wind_deg": int(message["wind_direction"]),
+            "precipitation": float(message["precipitation"]),
+        }
 
-    return lat, lon, y, timestamp, x
+        return lat, lon, y, timestamp, x
+    except KeyError as e:
+        logging.error(f"Missing key in message: {e}")
+        return None
 
 
 def unix_to_hour_pol(time):
@@ -70,118 +78,142 @@ def unix_to_hour_pol(time):
 
 
 def update_model(model, id, prev_timestamp, timestamp):
-    time_id = timeslot_id(timestamp)
-    prev_timestamp_id = timeslot_id(prev_timestamp)
+    try:
+        time_id = timeslot_id(timestamp)
+        prev_timestamp_id = timeslot_id(prev_timestamp)
 
-    # get data from hadoop with timestamps between prev_timestamp and timestamp
-    data = []
-    X = []
-    Y = []
-    seen_timeslot_ids = set()
+        data = []
+        X = []
+        Y = []
+        seen_timeslot_ids = set()
 
-    for weather in data:
-        ts_id = timeslot_id(weather["dt"])
-        if (
-            ts_id not in seen_timeslot_ids
-            and ts_id > prev_timestamp_id
-            and ts_id < time_id
-        ):
-            seen_timeslot_ids.add(ts_id)
-            x = {
-                "dt": timestamp,
-                "temp": float(weather["temperature"]),
-                "pressure": int(weather["pressure"]),
-                "humidity": int(weather["humidity"]),
-                "wind_speed": float(weather["wind_speed"]),
-                "wind_deg": int(weather["wind_direction"]),
-                "precipitation": float(weather["precipitation"]),
-            }
-            y = int(weather["cloud_coverage"])
-            X.append(x)
-            Y.append(y)
+        # Assume data is fetched here
+        for weather in data:
+            ts_id = timeslot_id(weather["dt"])
+            if (
+                ts_id not in seen_timeslot_ids
+                and ts_id > prev_timestamp_id
+                and ts_id < time_id
+            ):
+                seen_timeslot_ids.add(ts_id)
+                x = {
+                    "dt": timestamp,
+                    "temp": float(weather["temperature"]),
+                    "pressure": int(weather["pressure"]),
+                    "humidity": int(weather["humidity"]),
+                    "wind_speed": float(weather["wind_speed"]),
+                    "wind_deg": int(weather["wind_direction"]),
+                    "precipitation": float(weather["precipitation"]),
+                }
+                y = int(weather["cloud_coverage"])
+                X.append(x)
+                Y.append(y)
 
-    if len(seen_timeslot_ids) == time_id - prev_timestamp_id - 1:
-        logging.info("All data available")
-    else:
-        logging.warning("Some data missing")
+        if len(seen_timeslot_ids) == time_id - prev_timestamp_id - 1:
+            logging.info("All data available")
+        else:
+            logging.warning("Some data missing")
 
-    for x, y in zip(X, Y):
-        model.learn_one(y, x)
+        for x, y in zip(X, Y):
+            model.learn_one(y, x)
+
+    except Exception as e:
+        logging.error(f"Error in update_model: {e}")
 
 
 def process_message(message):
-    logging.info(f"Processing message: {message}")
-    lat, lon, y, timestamp, x = extract_data(message)
+    try:
+        logging.info(f"Processing message: {message}")
+        lat, lon, y, timestamp, x = extract_data(message)
+        if lat is None:
+            return
 
-    # lat, lon = 51.7, 19.5
-    id = get_location_id(lat, lon)
+        id = get_location_id(lat, lon)
 
-    ## TODO: check if data is valid and act accordingly
+        if id is None:
+            logging.error(f"Location ID not found for lat: {lat}, lon: {lon}")
+            return
 
-    # get model
-    model_data = adls_client.load_pickled_model_from_container(
-        config.container_name, f"model_{id}.pkl"
-    )
+        model_data = adls_client.load_pickled_model_from_container(
+            config.container_name, f"model_{id}.pkl"
+        )
+        model, previous_timestamp, x_hist = (
+            model_data["model"],
+            model_data["timestamp"],
+            model_data["x_hist"],
+        )
 
-    model, previous_timestamp, x_hist = (
-        model_data["model"],
-        model_data["timestamp"],
-        model_data["x_hist"],
-    )
+        x["dt"] = unix_to_hour_pol(x["dt"])
 
-    # TODO: check if model is outdated and act accordingly
-    if timeslot_id(timestamp) == timeslot_id(previous_timestamp):
-        return
-    if timeslot_id(timestamp) < timeslot_id(previous_timestamp):
-        logging.info("Old data")
-        return
-    if timeslot_id(timestamp) != timeslot_id(previous_timestamp) + 1:
-        update_model(model, id, previous_timestamp, timestamp)
+        model.learn_one(y, x_hist[0])
 
-    # preprocess x
-    x["dt"] = unix_to_hour_pol(x["dt"])
+        x_hist = x_hist[1:] + [x]
+        forecast = (
+            np.clip(np.array(model.forecast(4 * 24, x_hist)), 0, 100)
+            .reshape(-1, 4)
+            .mean(axis=1)
+        )
 
-    # update model
-    model.learn_one(y, x_hist[0])
+        update_forecast_to_mysql(id, forecast)
+        save_model_and_forecast(id, model, x_hist, forecast, timestamp)
 
-    # forecast
-    x_hist = x_hist[1:] + [x]
-    forecast = (
-        np.clip(np.array(model.forecast(4 * 24, x_hist)), 0, 100)
-        .reshape(-1, 4)
-        .mean(axis=1)
-    )
+    except Exception as e:
+        logging.error(f"Error processing message: {e}")
 
-    # save forecast to ADLS & mysql
-    update_query = """
-        UPDATE cloud_cover_forecasts SET
-            forecast_hour_1 = %s, forecast_hour_2 = %s, forecast_hour_3 = %s, 
-            forecast_hour_4 = %s, forecast_hour_5 = %s, forecast_hour_6 = %s, 
-            forecast_hour_7 = %s, forecast_hour_8 = %s, forecast_hour_9 = %s, 
-            forecast_hour_10 = %s, forecast_hour_11 = %s, forecast_hour_12 = %s, 
-            forecast_hour_13 = %s, forecast_hour_14 = %s, forecast_hour_15 = %s, 
-            forecast_hour_16 = %s, forecast_hour_17 = %s, forecast_hour_18 = %s, 
-            forecast_hour_19 = %s, forecast_hour_20 = %s, forecast_hour_21 = %s, 
-            forecast_hour_22 = %s, forecast_hour_23 = %s, forecast_hour_24 = %s
-        WHERE location_id = %s
-    """
-    values = tuple(forecast) + (id,)
 
-    # Execute the query
-    with mysql_client as db:
-        db.execute(update_query, values)
+def update_forecast_to_mysql(id, forecast):
+    try:
+        update_query = """
+            UPDATE cloud_cover_forecasts SET
+                forecast_hour_1 = %s, forecast_hour_2 = %s, forecast_hour_3 = %s, 
+                forecast_hour_4 = %s, forecast_hour_5 = %s, forecast_hour_6 = %s, 
+                forecast_hour_7 = %s, forecast_hour_8 = %s, forecast_hour_9 = %s, 
+                forecast_hour_10 = %s, forecast_hour_11 = %s, forecast_hour_12 = %s, 
+                forecast_hour_13 = %s, forecast_hour_14 = %s, forecast_hour_15 = %s, 
+                forecast_hour_16 = %s, forecast_hour_17 = %s, forecast_hour_18 = %s, 
+                forecast_hour_19 = %s, forecast_hour_20 = %s, forecast_hour_21 = %s, 
+                forecast_hour_22 = %s, forecast_hour_23 = %s, forecast_hour_24 = %s
+            WHERE location_id = %s
+        """
+        values = tuple(forecast) + (id,)
 
-    # Save the model back to ADLS and the forecast to ADLS
-    model_data_pickle = pickle.dumps(
-        {"model": model, "timestamp": timestamp, "x_hist": x_hist}
-    )
-    adls_client.upload_pickle("models", f"model_{id}.pkl", model_data_pickle)
+        with mysql_client as db:
+            db.execute(update_query, values)
 
-    forecast_dict = {
-        "id": id,
-        "timestamp": timestamp,
-        "forecast": forecast.tolist(),
-    }
+    except Exception as e:
+        logging.error(f"Error updating forecast to MySQL: {e}")
+
+
+def save_model_and_forecast(id, model, x_hist, forecast, timestamp):
+    try:
+        model_data_pickle = pickle.dumps(
+            {"model": model, "timestamp": timestamp, "x_hist": x_hist}
+        )
+        adls_client.upload_pickle("models", f"model_{id}.pkl", model_data_pickle)
+
+        forecast_dict = {
+            "id": id,
+            "timestamp": timestamp,
+            "forecast": forecast.tolist(),
+        }
+        # Save the forecast in ADLS or other storage
+    except Exception as e:
+        logging.error(f"Error saving model and forecast: {e}")
+
+
+def get_location_id(lat, lon):
+    try:
+        query = "SELECT id FROM locations WHERE latitude = %s AND longitude = %s"
+        values = (lat, lon)
+        with mysql_client as db:
+            result = db.read(query, values)
+            if result:
+                return result[0]["id"]
+            else:
+                return None
+    except Exception as e:
+        logging.error(f"Error fetching location ID: {e}")
+        return None
 
 
 def run_consumer():
@@ -190,22 +222,18 @@ def run_consumer():
         while True:
             messages = kafka_consumer.consume_messages(timeout=1.0)
             for message in messages:
-                process_message(message)
+                message_queue.put(message)
+
+            # ThreadPoolExecutor
+            while not message_queue.empty():
+                message = message_queue.get()
+                executor.submit(process_message, message)
+
     except KeyboardInterrupt:
         logging.info("Stopping Kafka consumer...")
     finally:
         kafka_consumer.close()
-
-
-def get_location_id(lat, lon):
-    query = "SELECT id FROM locations WHERE latitude = %s AND longitude = %s"
-    values = (lat, lon)
-    with mysql_client as db:
-        result = db.read(query, values)
-        if result:
-            return result[0]["id"]
-        else:
-            return None
+        executor.shutdown()
 
 
 if __name__ == "__main__":
