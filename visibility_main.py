60a3886bdc8ad7544443fa0b7d4d3974069106c5
jedrzejrucinski <jedrek.rucinski@gmail.com>
2025-01-19 16:10:32 +0100

    fix

--- visibility_main.py
+++ visibility_main.py
@@ -50,7 +50,7 @@ def get_satellite_trajectory(satellite: Satellite) -> List[SatelliteTrajectory]:
         data = db.read(query, values)
     if not data:
         raise HTTPException(status_code=404, detail="Satellite not found")
-    return [SatelliteTrajectory(**data[e]) for e in data]
+    return [SatelliteTrajectory(**data[e]) for e in len(data)]
 
 
 def get_satellites_in_time_range(start_time: int, end_time: int) -> list[Satellite]:
