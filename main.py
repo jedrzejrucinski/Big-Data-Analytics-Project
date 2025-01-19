d5c387dc972a53cb78deafe608a2c73c02c7becc
Adamo <czerwonskiadam@gmail.com>
2025-01-19 18:18:58 +0100

    update main

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from config import EnvConfig
 import numpy as np
 import pickle
 import math
+import datetime
+import pytz
+
+POF = 1737288000
+
+def timeslot_id(timestamp):
+    return math.floor((timestamp - POF) / 60*15)
 
 # Configure logging
 logging.basicConfig(
@@ -33,37 +40,97 @@ mysql_client = MySQLClient(
     "weather_db",
 )
 
+def extract_data(message):
+    message = json.loads(message)
+    lat, lon = float(message['latitude']), float(message['longitude'])  #this will work but for now it wont match values in the table
+    y = int(message["cloud_coverage"])
+    timestamp = int(message["timestamp"])
+    
+    x = {"dt": timestamp,
+         "temp": float(message["temperature"]),
+         "pressure": int(message["pressure"]),
+         "humidity": int(message["humidity"]),
+         "wind_speed": float(message["wind_speed"]),
+         "wind_deg": int(message["wind_direction"]),
+         "precipitation": float(message["precipitation"])}
+    
+    return lat, lon, y, timestamp, x
+
+def unix_to_hour_pol(time):
+    poland_tz = pytz.timezone('Europe/Warsaw')
+    return datetime.datetime.fromtimestamp(time, poland_tz).hour
+
+def update_model(model, id, prev_timestamp, timestamp):
+    time_id = timeslot_id(timestamp)
+    prev_timestamp_id = timeslot_id(prev_timestamp)
+
+    # get data from hadoop with timestamps between prev_timestamp and timestamp 
+    data = []
+    X = []
+    Y = []
+    seen_timeslot_ids = set()
+
+    for weather in data:
+        ts_id = timeslot_id(weather['dt'])
+        if ts_id not in seen_timeslot_ids and ts_id > prev_timestamp_id and ts_id < time_id:
+            seen_timeslot_ids.add(ts_id)
+            x = {"dt": timestamp,
+                "temp": float(weather["temperature"]),
+                "pressure": int(weather["pressure"]),
+                "humidity": int(weather["humidity"]),
+                "wind_speed": float(weather["wind_speed"]),
+                "wind_deg": int(weather["wind_direction"]),
+                "precipitation": float(weather["precipitation"])}
+            y = int(weather["cloud_coverage"])
+            X.append(x)
+            Y.append(y)
+    
+    if len(seen_timeslot_ids) == time_id - prev_timestamp_id - 1:
+        logging.info("All data available")
+    else:
+        logging.warning("Some data missing")
+    
+    for x, y in zip(X, Y):
+        model.learn_one(y, x)
+    
+
+
 
 def process_message(message):
     logging.info(f"Processing message: {message}")
-    message = json.loads(message)
-    # lat, lon = float(message['latitude']), float(message['longitude'])  this will work but for now it wont match values in the table
-    lat, lon = 51.7, 19.5
+    lat, lon, y, timestamp, x = extract_data(message)
+
+    #lat, lon = 51.7, 19.5
     id = get_location_id(lat, lon)
-    logging.info(f"Location id: {id}")
-
-    logging.info("Preprocessing data")
-    y = int(message["cloud_coverage"])
-
-    if np.isnan(y):
-        return
-
-    logging.info(f"Cloud coverage: {y}")
-    # preprocessing X here, not relevant right now
+    
+    ## TODO: check if data is valid and act accordingly
 
     # get model
-    model = adls_client.load_pickled_model_from_container(
+    model_data = adls_client.load_pickled_model_from_container(
         config.container_name, f"model_{id}.pkl"
     )
-    # fit
-    logging.info("Fitting model")
 
-    model.learn_one(int(y))
+    model, previous_timestamp, x_hist = model_data['model'], model_data['timestamp'], model_data["x_hist"]
+
+    # TODO: check if model is outdated and act accordingly
+    if timeslot_id(timestamp) == timeslot_id(previous_timestamp):
+        return
+    if timeslot_id(timestamp) < timeslot_id(previous_timestamp):
+        logging.info("Old data")
+        return
+    if timeslot_id(timestamp) != timeslot_id(previous_timestamp) + 1:
+        update_model(model, id, previous_timestamp, timestamp)
+
+    # preprocess x
+    x['dt'] = unix_to_hour_pol(x['dt'])
+
+    # update model
+    model.learn_one(y, x_hist[0])
 
     # forecast
-    logging.info("Forecasting")
+    x_hist = x_hist[1:] + [x]
     forecast = (
-        np.clip(np.array(model.forecast(4 * 24)), 0, 100).reshape(-1, 4).mean(axis=1)
+        np.clip(np.array(model.forecast(4 * 24, x_hist)), 0, 100).reshape(-1, 4).mean(axis=1)
     )
 
     # save forecast to ADLS & mysql
@@ -79,28 +146,21 @@ def process_message(message):
             forecast_hour_22 = %s, forecast_hour_23 = %s, forecast_hour_24 = %s
         WHERE location_id = %s
     """
-
-    # Example data for insertion
     values = tuple(forecast) + (id,)
-    logging.info(f"Forecast values: {values}")
 
     # Execute the query
     with mysql_client as db:
         db.execute(update_query, values)
 
     # Save the model back to ADLS and the forecast to ADLS
-    model_pickle = pickle.dumps(model)
-    adls_client.upload_pickle("models", f"model_{id}.pkl", model_pickle)
+    model_data_pickle = pickle.dumps({"model": model, "timestamp": timestamp, "x_hist": x_hist})
+    adls_client.upload_pickle("models", f"model_{id}.pkl", model_data_pickle)
 
     forecast_dict = {
         "id": id,
-        "timestamp": int(message["timestamp"]),
+        "timestamp": timestamp,
         "forecast": forecast.tolist(),
     }
-    adls_client.upload_dict_as_json(
-        "historical-forcasts", f"{id}-{message['timestamp']}.json", forecast_dict
-    )
-
 
 def run_consumer():
     try:
